93619e878845dbca4fcd16f9035f3f36b799f02b
Tran Dinh Huy <104847278+allenmeou@users.noreply.github.com>
2023-08-03 11:45:16 +0700

    upload code video 2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>hello world with React</h2>
+        <h3>test </h3>
       </header>
     </div>
   );
