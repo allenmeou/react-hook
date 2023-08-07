56da79cd1a47e9425a0f8dbb3684c61edecdc75d
Tran Dinh Huy <104847278+allenmeou@users.noreply.github.com>
2023-08-07 10:12:04 +0700

    upload code video #8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 // import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 import Nav from "./views/Nav";
 
 const App = () => {
+  // STATE
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+
+  // EVENT
+  const handleSubmitButton = () => {
+    setName(address);
+  };
+  const handleChangeValueInput = (event) => {
+    setAddress(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <Nav />
-        hello
+        <h3>Hello world with {name}</h3>
+        <input
+          type="text"
+          value={address}
+          onChange={(event) => handleChangeValueInput(event)}
+        />{" "}
+        <br />
+        <button type="button" onClick={() => handleSubmitButton()}>
+          CLick Me!
+        </button>
+        <button></button>
       </header>
     </div>
   );
--- src/views/Nav.js
+++ src/views/Nav.js
@@ -1,4 +1,4 @@
-import "./Nav.css";
+import "./Nav.scss";
 import logo from "../logo.svg";
 const Nav = () => {
   return (
