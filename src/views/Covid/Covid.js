2aaf270f66309d17fbe4220ed5bf762dff38d118
Tran Dinh Huy <104847278+allenmeou@users.noreply.github.com>
2023-08-11 11:15:49 +0700

    upload code video #17 -- Loading Message

--- src/views/Covid/Covid.js
+++ src/views/Covid/Covid.js
@@ -5,35 +5,39 @@ import "./Covid.scss";
 
 const Covid = () => {
   const [dataCovid, setDataCovid] = useState([]);
+  const [loading, setLoading] = useState(true);
   // componentDidMount
   useEffect(() => {
-    (async function anyNameFunction() {
-      const options = {
-        method: "GET",
-        url: "https://covid-193.p.rapidapi.com/statistics",
-        headers: {
-          "X-RapidAPI-Key":
-            "420e0a031amsh6ce33aa56b81f45p1feb8cjsnde5ededbc0fe",
-          "X-RapidAPI-Host": "covid-193.p.rapidapi.com",
-        },
-      };
+    setTimeout(() => {
+      (async function anyNameFunction() {
+        const options = {
+          method: "GET",
+          url: "https://covid-193.p.rapidapi.com/statistics",
+          headers: {
+            "X-RapidAPI-Key":
+              "420e0a031amsh6ce33aa56b81f45p1feb8cjsnde5ededbc0fe",
+            "X-RapidAPI-Host": "covid-193.p.rapidapi.com",
+          },
+        };
 
-      try {
-        const response = await axios.request(options);
-        let dataRes = response.data.response;
-        if (dataRes && dataRes.length > 9) {
-          dataRes.map((item) => {
-            item.time = moment(item.time).format("DD/MM/YYYY");
-            return item;
-          });
+        try {
+          const response = await axios.request(options);
+          let data = response.data.response;
+          if (data && data.length > 9) {
+            data.map((item) => {
+              item.time = moment(item.time).format("DD/MM/YYYY");
+              return item;
+            });
+          }
+          // console.log(data);
+          setDataCovid(data);
+          setLoading(false);
+          // console.log(">>> check setDataCovid", dataCovid);
+        } catch (error) {
+          console.error(error);
         }
-        console.log(dataRes);
-        setDataCovid(dataRes);
-        console.log(">>> check setDataCovid", dataCovid);
-      } catch (error) {
-        console.error(error);
-      }
-    })();
+      })();
+    }, 4000);
   }, []);
 
   return (
@@ -48,20 +52,30 @@ const Covid = () => {
             <th>Deaths</th>
             <th>Recovered</th>
           </tr>
-          {dataCovid.map((item, index) => {
-            return (
-              <tr key={index}>
-                <td>{item.time}</td>
-                <td>{item.country}</td>
-                {/* <td></td>
+          {loading === false &&
+            dataCovid &&
+            dataCovid.length > 0 &&
+            dataCovid.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{item.time}</td>
+                  <td>{item.country}</td>
+                  {/* <td></td>
                 <td></td>
                 <td></td> */}
-                <td>{item.cases.active}</td>
-                <td>{item.deaths.total}</td>
-                <td>{item.cases.recovered}</td>
-              </tr>
-            );
-          })}
+                  <td>{item.cases.active}</td>
+                  <td>{item.deaths.total}</td>
+                  <td>{item.cases.recovered}</td>
+                </tr>
+              );
+            })}
+          {loading === true && (
+            <tr>
+              <td style={{ textAlign: "center" }} colSpan="5">
+                Loading ...
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
