156c433d512bff68423a138849231cf278dff97a
Tran Dinh Huy <104847278+allenmeou@users.noreply.github.com>
2023-08-15 11:14:23 +0700

    upload code video @21 --Fix-Link React Router Dom

--- src/views/Countdown.js
+++ src/views/Countdown.js
@@ -1,65 +1,55 @@
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 class CountDown extends React.Component {
+  state = {
+    count: 10,
+  };
 
-    state = {
-        count: 10
+  componentDidMount() {
+    // setTimeout(() => {
+    //     console.log('me')
+    // }, 1000);
+
+    this.timer = setInterval(() => {
+      this.setState({
+        count: this.state.count - 1,
+      });
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count && this.state.count === 0) {
+      if (this.timer) {
+        clearInterval(this.timer);
+        // this.props.onTimesup();
+      }
     }
+  }
 
-
-    componentDidMount() {
-        // setTimeout(() => {
-        //     console.log('me')
-        // }, 1000);
-
-        this.timer = setInterval(() => {
-            this.setState({
-                count: this.state.count - 1
-            })
-        }, 1000);
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.count !== this.state.count && this.state.count === 0) {
-            if (this.timer) {
-                clearInterval(this.timer);
-                // this.props.onTimesup();
-            }
-        }
-    }
-
-    render() {
-        return (
-            <div>{this.state.count} class</div>
-        )
-    }
-
+  render() {
+    return <div>{this.state.count} class</div>;
+  }
 }
-
 
 const NewCountDown = (props) => {
-    const [count, setCount] = useState(10);
+  const [count, setCount] = useState(10);
 
-    useEffect(() => {
-        if (count === 0) {
-            props.onTimesup();
-            return;
-        }
+  useEffect(() => {
+    if (count === 0) {
+      props.onTimesup();
+      return;
+    }
 
-        let timer = setInterval(() => {
-            setCount(count - 1);
-        }, 1000);
+    let timer = setInterval(() => {
+      setCount(count - 1);
+    }, 1000);
 
-        return () => {
-            clearInterval(timer)
-        }
-    }, [count]);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [count, props]);
 
-    return (
-        <div>{count} hooks</div>
-    )
-}
-
+  return <div>{count} hooks</div>;
+};
 
 export { CountDown, NewCountDown };
--- src/views/Nav.js
+++ src/views/Nav.js
@@ -1,15 +1,23 @@
-import '../views/Nav.scss';
-import { Link, NavLink } from 'react-router-dom';
+import "../views/Nav.scss";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <div className="topnav">
-            <NavLink activeClassName="active1" to="/" exact>Home</NavLink>
-            <NavLink activeClassName="active1" to="/timer">Timer Apps</NavLink>
-            <NavLink activeClassName="active1" to="/todo">Todo Apps</NavLink>
-            <NavLink activeClassName="active1" to="/secret">Secret</NavLink>
-        </div>
-    );
-}
+  return (
+    <div className="topnav">
+      <NavLink activeClassName="active1" to="/" exact>
+        Home
+      </NavLink>
+      <NavLink activeClassName="active1" to="/timer">
+        Timer Apps
+      </NavLink>
+      <NavLink activeClassName="active1" to="/todo">
+        Todo Apps
+      </NavLink>
+      <NavLink activeClassName="active1" to="/secret">
+        Secret
+      </NavLink>
+    </div>
+  );
+};
 
 export default Nav;
