543f07437408b5afccf7b6b6ebb318674e2f362f
Tran Dinh Huy <104847278+allenmeou@users.noreply.github.com>
2023-08-19 15:27:34 +0700

    upload code video #25  --create blog detail

--- src/views/DetailBlog.js
+++ src/views/DetailBlog.js
@@ -1,19 +1,40 @@
 import { useParams, useHistory } from "react-router-dom";
+import useFetch from "../customize/fetchBlog";
 
 import "./DatailBlog.scss";
 
 const DetailBlog = () => {
+  let { id } = useParams();
+
   let history = useHistory();
+
   function handleClickBackBlog() {
     history.push("/blog");
   }
 
-  let { id } = useParams();
+  const { data: dataBlogDetail, loading: setLoading } = useFetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    false
+  );
 
   return (
     <>
       <button onClick={handleClickBackBlog}>Back</button>
-      <h1>hello detail blog with {id}</h1>
+      <div className="blog-detail">
+        {setLoading === false && dataBlogDetail && (
+          <>
+            <div className="title">
+              Blog ID : {id} - {dataBlogDetail.title}
+            </div>
+            <div className="content">{dataBlogDetail.body}</div>
+          </>
+        )}
+        {setLoading === true && (
+          <>
+            <span style={{ margin: "0 auto" }}>loading ...</span>
+          </>
+        )}
+      </div>
     </>
   );
 };
